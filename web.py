012570709c93ae13bac2df1d7461421699d4271b
Rfer7114 <russel.workid@gmail.com>
2023-11-10 08:41:43 +0100

    testing push to new branch on remote repository test branch

--- web.py
+++ web.py
@@ -14,12 +14,16 @@ st.title("My Todo App")
 st.subheader("Add, delete, edit your list")
 st.write("My Current List:")
 
-for todo_list in todos:
-    st.checkbox(todo_list, key=todo_list)
+for index, todo_list in enumerate(todos):
+    checkbox = st.checkbox(todo_list, key=todo_list)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo_list]
+        st.rerun()
 
 st.text_input(label="Add Item", label_visibility='hidden', placeholder="Add a todo",
               on_change=add_todo, key='new_todo')
 
-print('Hello')
 st.session_state
 
